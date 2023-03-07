41f3c10a2b364b5bc7c5d7afab3776e2ebba2715
Niklas Schröder <niklas.schroeder@digitalservice.bund.de>
2023-03-07 20:45:17 +0100

    Remove old date selector fields

--- src/lib/Troi.svelte
+++ src/lib/Troi.svelte
@@ -74,53 +74,6 @@
     {selectedDateChanged}
     {weekChanged}
   />
-  <div class="flex gap-4">
-    <div class="py-4">
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label class="text-sm"
-        >Show from:
-        <DateInput
-          bind:value={startDate}
-          format="yyyy-MM-dd"
-          closeOnSelection={true}
-        />
-      </label>
-    </div>
-
-    <div class="inline-block py-4">
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label class="text-sm"
-        >to:
-        <DateInput
-          bind:value={endDate}
-          format="yyyy-MM-dd"
-          closeOnSelection={true}
-        />
-      </label>
-    </div>
-    <div class="inline-block content-center pt-8">
-      <button
-        class="rounded-sm border border-blue-500 px-2 py-1 hover:bg-blue-100"
-        on:click={() => {
-          (projects = []), reload();
-        }}
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="#0063eb"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-          />
-        </svg></button
-      >
-    </div>
-  </div>
 </section>
 
 {#each projects as project}
