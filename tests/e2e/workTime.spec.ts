3895fac912c62a417a40aca66face31f57a90523
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-04-04 13:21:43 +0200

    Refactor work time test setup

--- tests/e2e/workTime.spec.ts
+++ tests/e2e/workTime.spec.ts
@@ -1,10 +1,22 @@
 import { type Page, expect, test } from "@playwright/test";
 import LoginPage from "./LoginPage";
 
-test.describe("work time actions", () => {
+test.describe("work time form", () => {
   test.beforeEach(async ({ page }) => {
     await new LoginPage(page).logIn("max.mustermann", "aSafePassword");
   });
+
+  async function fillWorkTimeFormAndSubmit(
+    page: Page,
+    startTime: string,
+    breakTime: string,
+    endTime: string,
+  ) {
+    await page.getByLabel("Start time").fill(startTime);
+    await page.getByLabel("Break").fill(breakTime);
+    await page.getByLabel("End time").fill(endTime);
+    await clickWorkTimeButton(page, "Save");
+  }
 
   function clickWorkTimeButton(page: Page, buttonText: string) {
     return page
@@ -14,31 +26,19 @@ test.describe("work time actions", () => {
   }
 
   test("should add, edit and delete work time", async ({ page }) => {
-    // Input start time, break and end time and submit form
-    await page.getByLabel("Start time").fill("08:00");
-    await page.getByLabel("Break").fill("01:00");
-    await page.getByLabel("End time").fill("17:00");
-    await clickWorkTimeButton(page, "Save");
-
-    // Check if the new work time is visible
+    await fillWorkTimeFormAndSubmit(page, "08:00", "01:00", "17:00");
     await expect(page.getByText("8:00")).toBeVisible();
 
     await clickWorkTimeButton(page, "Edit");
-
-    // Change the start time and submit the form
     await page.getByLabel("Start time").fill("09:00");
     await clickWorkTimeButton(page, "Update");
-
-    // Check if the new work time is visible
     await expect(page.getByText("7:00")).toBeVisible();
 
     await clickWorkTimeButton(page, "Delete");
-
-    // Check if the work time is no longer visible
     await expect(page.getByText("7:00")).not.toBeVisible();
   });
 
-  test("work time form should be disabled after submitting, enabled after edit, disabled after cancel or update and enabled after deleting", async ({
+  test("should be disabled after submitting, enabled after edit, disabled after cancel or update and enabled after deleting", async ({
     page,
   }) => {
     function expectFormToBeDisabled() {
@@ -53,12 +53,7 @@ test.describe("work time actions", () => {
       }
     }
 
-    // Input start time, break and end time and submit form
-    await page.getByLabel("Start time").fill("08:00");
-    await page.getByLabel("Break").fill("01:00");
-    await page.getByLabel("End time").fill("17:00");
-
-    await clickWorkTimeButton(page, "Save");
+    await fillWorkTimeFormAndSubmit(page, "08:00", "01:00", "17:00");
     expectFormToBeDisabled();
 
     await clickWorkTimeButton(page, "Edit");
