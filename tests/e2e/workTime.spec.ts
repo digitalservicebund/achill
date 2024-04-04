2655dc70f0775964555314e09821b3bce3ba306b
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-04-04 13:02:31 +0200

    Refactor test and add more form interactions

--- tests/e2e/workTime.spec.ts
+++ tests/e2e/workTime.spec.ts
@@ -1,65 +1,79 @@
-import { expect, test } from "@playwright/test";
+import { type Page, expect, test } from "@playwright/test";
 import LoginPage from "./LoginPage";
 
 test.describe("work time actions", () => {
-  test("should add, edit and delete work time", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await new LoginPage(page).logIn("max.mustermann", "aSafePassword");
+  });
 
+  function clickWorkTimeButton(page: Page, buttonText: string) {
+    return page
+      .locator("#work-time-form button")
+      .filter({ hasText: buttonText })
+      .click();
+  }
+
+  test("should add, edit and delete work time", async ({ page }) => {
     // Input start time, break and end time and submit form
     await page.getByLabel("Start time").fill("08:00");
     await page.getByLabel("Break").fill("01:00");
     await page.getByLabel("End time").fill("17:00");
-    await page
-      .locator("#work-time-form button")
-      .filter({ hasText: "Save" })
-      .click();
+    await clickWorkTimeButton(page, "Save");
 
     // Check if the new work time is visible
     await expect(page.getByText("8:00")).toBeVisible();
 
-    // Click on the edit button
-    await page
-      .locator("#work-time-form button")
-      .filter({ hasText: "Edit" })
-      .click();
+    await clickWorkTimeButton(page, "Edit");
 
     // Change the start time and submit the form
     await page.getByLabel("Start time").fill("09:00");
-    await page
-      .locator("#work-time-form button")
-      .filter({ hasText: "Update" })
-      .click();
+    await clickWorkTimeButton(page, "Update");
 
     // Check if the new work time is visible
     await expect(page.getByText("7:00")).toBeVisible();
 
-    // Click on the delete button
-    await page
-      .locator("#work-time-form button")
-      .filter({ hasText: "Delete" })
-      .click();
+    await clickWorkTimeButton(page, "Delete");
 
     // Check if the work time is no longer visible
     await expect(page.getByText("7:00")).not.toBeVisible();
   });
 
-  test("work time form should be disabled after submitting", async ({
+  test("work time form should be disabled after submitting, enabled after edit, disabled after cancel or update and enabled after deleting", async ({
     page,
   }) => {
-    await new LoginPage(page).logIn("max.mustermann", "aSafePassword");
+    function expectFormToBeDisabled() {
+      for (const label of ["Start time", "Break", "End time"]) {
+        expect(page.getByLabel(label)).toBeDisabled();
+      }
+    }
+
+    function expectFormToBeEnabled() {
+      for (const label of ["Start time", "Break", "End time"]) {
+        expect(page.getByLabel(label)).toBeEnabled();
+      }
+    }
 
     // Input start time, break and end time and submit form
     await page.getByLabel("Start time").fill("08:00");
     await page.getByLabel("Break").fill("01:00");
     await page.getByLabel("End time").fill("17:00");
-    await page
-      .locator("#work-time-form button")
-      .filter({ hasText: "Save" })
-      .click();
 
-    // Check if the form is disabled
-    for (const label of ["Start time", "Break", "End time"]) {
-      await expect(page.getByLabel(label)).toBeDisabled();
-    }
+    await clickWorkTimeButton(page, "Save");
+    expectFormToBeDisabled();
+
+    await clickWorkTimeButton(page, "Edit");
+    expectFormToBeEnabled();
+
+    await clickWorkTimeButton(page, "Update");
+    expectFormToBeDisabled();
+
+    await clickWorkTimeButton(page, "Edit");
+    expectFormToBeEnabled();
+
+    await clickWorkTimeButton(page, "Cancel");
+    expectFormToBeDisabled();
+
+    await clickWorkTimeButton(page, "Delete");
+    expectFormToBeEnabled();
   });
 });
