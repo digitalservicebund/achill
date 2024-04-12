8cd85a369108675fbbd0d367bb8e6d5c83a9ca8d
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-04-12 15:21:56 +0200

    Don't run tests in parallel
    Prevents race conditions of the mocked APIs

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -21,8 +21,6 @@ const baseURL = useDefaultBaseUrl
 export default defineConfig({
   timeout: 10000,
   testDir: ".",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
