534fc3d7cacf4bee96ae0d21d8166de533b82b83
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-04-12 15:22:03 +0200

    Reuse baseUrl

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "MOCK_EXTERNAL_APIS=true npm run dev",
-    url: "http://127.0.0.1:3000",
+    url: baseURL,
     reuseExistingServer: false,
     // stdout: "pipe",
   },
