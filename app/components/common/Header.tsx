cfb3a88787b7b5ad0f370f94d8e4f29985f6970f
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-18 11:15:35 +0200

    Fix icon in header

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -3,11 +3,7 @@ import { Form } from "@remix-run/react";
 export default function Header({ username }: { username?: string }) {
   return (
     <nav className="flex h-16 justify-between items-center border-1 w-full border-b pb-1 text-left">
-      <img
-        src="timetracking_blue.svg"
-        alt="Track-Your-Time logo"
-        className="pr-4 py-1"
-      />
+      <img src="favicon.svg" alt="Track-Your-Time logo" className="pr-4 py-1" />
       <div className="flex-grow">
         <h1 className="font-bold">Track-Your-Time</h1>
         {username && <div className="text-sm">Logged in as {username}.</div>}
